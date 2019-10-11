08c5ffd93e0373ae2d8c1879323c3e959a03cc06
Ritesh Kumar <ritesh705@gmail.com>
2019-10-11 10:13:08 +0530

    added - file for queue

---
+++ src/main/java/com/ritesh/queue/SlidingWindowMaximum.java
@@ -0,0 +1,16 @@
+package com.ritesh.queue;
+
+/*
+Description - Given an array and an integer k, find the maximum for each and every contiguous subarray of size k.
+Input  :
+arr[] = {1, 2, 3, 1, 4, 5, 2, 3, 6}, k = 3
+
+Output :
+3 3 4 5 5 5 6
+
+@author :- Ritesh Kumar | ritesh705@gmail.com
+*/
+public class SlidingWindowMaximum
+{
+    // TODO
+}
