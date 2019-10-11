0474aba35d109548053bf9af43370fdcc569a030
Ritesh Kumar <ritesh705@gmail.com>
2019-10-12 03:21:23 +0530

    update - logic added for sliding window maximum program

--- src/main/java/com/ritesh/queue/SlidingWindowMaximum.java
+++ src/main/java/com/ritesh/queue/SlidingWindowMaximum.java
@@ -1,5 +1,10 @@
 package com.ritesh.queue;
 
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
 /*
 Description - Given an array and an integer k, find the maximum for each and every contiguous subarray of size k.
 Input  :
@@ -12,5 +17,40 @@ Output :
 */
 public class SlidingWindowMaximum
 {
-    // TODO
+
+    private static List getSlidingWindowMax(int[] arr, int k)
+    {
+        List result = new ArrayList();
+        Deque<Integer> deque = new LinkedList();
+        for(int i=0; i<arr.length; i++)
+        {
+            while(!deque.isEmpty() && arr[i]>=arr[deque.peekLast()])
+            {
+                deque.pollLast();
+            }
+            deque.addLast(i);
+
+            while(deque.size() > k)
+            {
+                deque.pollFirst();
+            }
+
+            if(i>=k-1)
+            {
+                result.add(arr[deque.getFirst()]);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args)
+    {
+        int[] arr = {1, 2, 3, 1, 4, 5, 2, 3, 6, 5, 3, 2};
+        List<Integer> slidingWindowMax = getSlidingWindowMax(arr, 3);
+        for(Integer val : slidingWindowMax)
+        {
+            System.out.println(val);
+        }
+    }
+
 }
