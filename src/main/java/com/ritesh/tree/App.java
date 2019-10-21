aff92502e34f970970b0f9960f8965f541c579c1
Ritesh Kumar <ritesh705@gmail.com>
2019-10-21 13:58:33 +0530

    added - LCA program

--- src/main/java/com/ritesh/tree/App.java
+++ src/main/java/com/ritesh/tree/App.java
@@ -4,6 +4,7 @@ import com.ritesh.tree.core.Node;
 import com.ritesh.tree.core.Tree;
 import com.ritesh.tree.deletetion.Deletion;
 import com.ritesh.tree.insertion.Insertion;
+import com.ritesh.tree.lca.LowestCommonAncestor;
 import com.ritesh.tree.traversal.TreeTraversal;
 
 public class App
@@ -16,6 +17,8 @@ public class App
         tree.root.right = new Node(3);
         tree.root.left.left = new Node(4);
         tree.root.left.right = new Node(5);
+        tree.root.right.left = new Node(6);
+        tree.root.right.right = new Node(7);
 
         System.out.println("PreOrder traversal of binary tree is ");
         TreeTraversal.preOrder(tree.root);
@@ -29,14 +32,17 @@ public class App
         System.out.println("LevelOrder traversal of binary tree is ");
         TreeTraversal.levelOrder(tree.root);
 
-        Insertion.insert(tree.root, 6);
-        Insertion.insert(tree.root, 7);
+        // Insertion
+        Insertion.insert(tree.root, 8);
+        Insertion.insert(tree.root, 9);
 
         System.out.println("LevelOrder traversal of binary tree is ");
         TreeTraversal.levelOrder(tree.root);
 
-        Deletion.delete(tree.root, 6);
-        System.out.println("LevelOrder traversal of binary tree is ");
-        TreeTraversal.levelOrder(tree.root);
+        // Deletion
+        Deletion.delete(tree.root, 9);
+
+        // Finding LCA
+        System.out.println("LCA(2, 4) = " + LowestCommonAncestor.findLCA(tree.root, 2, 4).key);
     }
 }
