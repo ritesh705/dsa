f8b89bf8ad37e8535401398bf5b18a18846d2a26
Ritesh Kumar <ritesh705@gmail.com>
2019-12-29 12:56:02 +0530

    added - code for bst-lca

---
+++ src/main/java/com/ritesh/bst/lca/LowestCommonAncestor.java
@@ -0,0 +1,33 @@
+package com.ritesh.bst.lca;
+
+import com.ritesh.tree.core.Node;
+
+/*
+ * @author: Ritesh Kumar | ritesh705@gmail.com
+ * @since: 29th December, 2019
+ * */
+
+public class LowestCommonAncestor
+{
+    public static Node findLCA(Node root, int n1, int n2)
+    {
+        Node lcaNode = null;
+        if(root == null)
+        {
+            lcaNode = null;
+        }
+        else if(n1 > root.key && n2 > root.key)
+        {
+            lcaNode = findLCA(root.right, n1, n2);
+        }
+        else if(n1 < root.key && n2 < root.key)
+        {
+            lcaNode = findLCA(root.left, n1, n2);
+        }
+        else
+        {
+            lcaNode = root;
+        }
+        return lcaNode;
+    }
+}
