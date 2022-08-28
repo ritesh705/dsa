748d331587bc2a66c723623ef188218d364565a6
Ritesh Kumar <ritesh705@gmail.com>
2022-08-29 01:08:37 +0530

    update

--- src/main/java/com/ritesh/test/Test.java
+++ src/main/java/com/ritesh/test/Test.java
@@ -9,10 +9,8 @@ This class is for only testing small code snippet
 * */
 public class Test
 {
-    static final int[] testArr = {1, 2};
-
-    public static void main(String[] args) {
-        Integer[] testArr = {1, 2};
+    public static void main(String[] args)
+    {
         List<Integer> testList = Collections.unmodifiableList(Arrays.asList(1, 2));
         System.out.println(testList.get(1));
     }
