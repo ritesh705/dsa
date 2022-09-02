e7a8c93c3809ac1f1e2de006ae26f85e702761aa
Ritesh Kumar <ritesh705@gmail.com>
2022-09-02 14:38:42 +0530

    update

--- src/main/java/com/ritesh/test/Test.java
+++ src/main/java/com/ritesh/test/Test.java
@@ -7,7 +7,8 @@ import java.util.List;
 /*
 This class is for only testing small code snippet
 * */
-public class Test{
+public class Test
+{
     public static void main(String[] args)
     {
         System.out.println("Test Class");
