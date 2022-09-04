f354ca3912596325c6db44591ea722f884d59373
Ritesh Kumar <ritesh705@gmail.com>
2022-09-04 16:08:48 +0530

    update - best time to buy and sell stock

--- src/main/java/com/ritesh/ds/arrays/practice/StockBuyAndSellMaxProfit.java
+++ src/main/java/com/ritesh/ds/arrays/practice/StockBuyAndSellMaxProfit.java
@@ -1,21 +1,14 @@
 package com.ritesh.ds.arrays.practice;
 
 /*
-##### BEST TIME TO BUY AND SELL STOCK #####
-
-You are given an array prices where prices[i] is the price of a given stock on the ith day.
-
-You want to maximize your profit by choosing a single day to buy one stock
-and choosing a different day in the future to sell that stock.
-
-Return the maximum profit you can achieve from this transaction.
-If you cannot achieve any profit, return 0.
-* */
-public class StockBuyAndSellBestTime
+##### MAXIMIZE PROFIT | BUY AND SELL STOCK #####
+*/
+public class StockBuyAndSellMaxProfit
 {
     public static void main(String[] args)
     {
-        int[] priceArr = new int[]{100, 180, 260, 310, 40, 535, 695 };
+        int[] priceArr = new int[]{100, 180, 260, 310, 40, 535, 695};
+        priceArr = new int[]{7,1,5,3,6,4};
         System.out.println(getMaxProfit(priceArr));
     }
 
@@ -24,16 +17,26 @@ public class StockBuyAndSellBestTime
         int buyPrice = 0;
         int sellPrice = 0;
         int maxProfit = 0;
+        if(priceArr.length == 0)
+        {
+            return maxProfit;
+        }
         for(int i=0; i< priceArr.length; i++)
         {
-            if(i==0 && priceArr[i]<priceArr[i+1])
+            if(i==0)
             {
-                buyPrice = priceArr[i];
-                sellPrice = 0;
+                if(priceArr[i]<priceArr[i+1])
+                {
+                    buyPrice = priceArr[i];
+                    sellPrice = 0;
+                }
             }
-            else if(i==priceArr.length-1 && priceArr[i] > priceArr[i-1])
+            else if(i==priceArr.length-1)
             {
-                sellPrice = priceArr[i];
+                if(priceArr[i] > priceArr[i-1])
+                {
+                    sellPrice = priceArr[i];
+                }
             }
             else if(priceArr[i] < priceArr[i-1] && priceArr[i] < priceArr[i+1])
             {
@@ -47,6 +50,8 @@ public class StockBuyAndSellBestTime
             if(buyPrice > 0 && sellPrice > buyPrice)
             {
                 maxProfit = maxProfit + sellPrice-buyPrice;
+                buyPrice =0;
+                sellPrice =0;
             }
         }
         return maxProfit;
