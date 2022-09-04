cc51ba359acd9f514ef1cabf2e8ed16308066313
Ritesh Kumar <ritesh705@gmail.com>
2022-09-04 14:16:44 +0530

    update

--- src/main/java/com/ritesh/test/Test.java
+++ src/main/java/com/ritesh/test/Test.java
@@ -1,16 +1,11 @@
 package com.ritesh.test;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 /*
 This class is for only testing small code snippet
 * */
 public class Test
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args){
         System.out.println("Test Class");
     }
 }
