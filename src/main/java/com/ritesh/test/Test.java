d74dc97f38c9990840cfbe4a0b9501377d743625
Ritesh Kumar <ritesh705@gmail.com>
2023-05-05 12:52:36 +0530

    update

--- src/main/java/com/ritesh/test/Test.java
+++ src/main/java/com/ritesh/test/Test.java
@@ -5,7 +5,8 @@ This class is for only testing small code snippet
 * */
 public class Test
 {
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
         System.out.println("Test Class");
     }
 }
